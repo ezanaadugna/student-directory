7cc0e0504e8622a41482e449d94a54e3db404bdd
Ezana Adugna <eadugna3005@gmail.com>
2023-05-05 11:56:08 +0100

    directory 6

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
 # first, we print the list of students
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-  ]
+
+def input_students
+    puts  "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+#empty array for names/cohort
+    students = []
+#get the first name
+    name = gets.chomp
+#while the name is not empty, repeat this code
+    while !name.empty? do    
+        students << {name: name, cohort: :november}
+        name = gets.chomp
+    end
+    return students
+end
+
 
 def print_header
     puts "The students of Villains Academy"
@@ -28,6 +31,7 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
