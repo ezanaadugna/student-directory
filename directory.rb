90b42cca40277acfe2ef46c3dadace721fc9879a
Ezana Adugna <eadugna3005@gmail.com>
2023-05-07 11:56:15 +0100

    eleven 1

--- directory.rb
+++ directory.rb
@@ -3,6 +3,7 @@
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit" # 9 because we'll be adding more items  
 end
 
@@ -41,15 +42,30 @@ def show_students
     print_footer
 end
 
+def save_students
+    file = File.open("students.csv.gitignore", "w")
+
+#iterating over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+  file.close
+end
+
+
 def process(selection)
     case selection
-      when "1"
-        input_students
-      when "2"
-        show_students
-      when "9"
-        exit
-      else
+        when "1"
+            input_students
+        when "2"
+            show_students
+        when "3"
+            save_students
+        when "9"
+            exit
+        else
         puts "I don't know what you mean, try again"
     end
 end
@@ -62,3 +78,4 @@ def interactive_menu
 end
 
 interactive_menu
+
